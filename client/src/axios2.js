3aeb20062d6a8ffaebf435ac7e3a97a5406eb908
Muhannad <muhannad.1design@gmail.com>
2024-12-04 15:17:16 +0300

    Updates

--- client/src/axios2.js
+++ client/src/axios2.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axios2 = axios.create({
-    url: process.env.REACT_APP_API_URL || "http://localhost:3001", // Dynamic URL
+    // url: "http://localhost:3001",       // Static URL
+    url: process.env.REACT_APP_API_URL, // Dynamic URL
 });
 
 export default axios2;
