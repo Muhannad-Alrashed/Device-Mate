070a7c032eb01b34deaff5618f1c68d7afca060c
Muhannad <muhannad.1design@gmail.com>
2024-12-08 16:18:31 +0300

    updates

--- client/src/context/webSocketContext.js
+++ client/src/context/webSocketContext.js
@@ -4,9 +4,8 @@ import axios2 from "../axios2";
 import { AuthContext } from "./authContext";
 
 const socket = io(
-    'https://device-mate.up.railway.app/'
-    // process.env.REACT_APP_API_URL             // Production
-    ,// || 'http://localhost:3001',             // Development
+    process.env.REACT_APP_API_URL           // Production
+    || 'http://localhost:3001',             // Development
     { transports: ["websocket", "polling"] });
 
 export const WebSocketContext = createContext();
